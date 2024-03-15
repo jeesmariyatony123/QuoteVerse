bec336ec2b450d2cf585b4aa071624009fab292e
jeesmariyatony123 <jeestony123@gmail.com>
2024-03-15 14:25:47 +0530

    Add share button using react-share

--- src/components/QuoteSection.jsx
+++ src/components/QuoteSection.jsx
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchQuotes } from '../REDUX/Slices/quoteSlice';
 import { addLiked } from '../REDUX/Slices/likedquoteSlice';
+import { TwitterShareButton } from "react-share";
 
 const QuoteSection = () => {
 
@@ -16,16 +17,18 @@ const QuoteSection = () => {
     dispatch(fetchQuotes())
   }, [])
 
-  const liked=useSelector(state=>state.likedquoteReducer)
+  const liked = useSelector(state => state.likedquoteReducer)
 
-  const handleLiked=(quote)=>{
-    if(liked?.includes(quote)){
+  const handleLiked = (quote) => {
+    if (liked?.includes(quote)) {
       alert("Quote already liked...")
-    }else{
+    } else {
       dispatch(addLiked(quote))
       alert("Liked")
     }
   }
+
+  const currentPageUrl = 'twitter.com';
 
   return (
     <div>
@@ -33,7 +36,7 @@ const QuoteSection = () => {
         <Row >
           <div >
 
-            {allQuotes?.map((quote,index) => (
+            {allQuotes?.map((quote, index) => (
               <div key={index} className='d-flex border-bottom border-secondary pt-4'>
                 <div>
                   <img src={image} />
@@ -43,9 +46,11 @@ const QuoteSection = () => {
                   <h6 style={{ fontSize: '16px' }}>{quote?.author}</h6>
                   <h6 className='text-white' style={{ fontSize: '15px' }}>{quote?.content}</h6>
 
-                  <button onClick={()=>handleLiked(quote)} className=' btn border-0 pe-1 ps-1 '><i className="fa-regular fa-heart"></i></button>
+                  <button onClick={() => handleLiked(quote)} className=' btn border-0 pe-1 ps-1 '><i className="fa-regular fa-heart"></i></button>
                   <button className='btn border-0 pe-1 '><i className="fa-regular fa-comment"></i></button>
-                  <button className='btn border-0 '><i className="fa-regular fa-paper-plane"></i></button>
+
+                  <TwitterShareButton url={currentPageUrl} quote="please share this post" hastag='#quote'><button className='btn border-0 '><i className="fa-regular fa-paper-plane"></i></button></TwitterShareButton>
+                  
                   <p className='pt-2' style={{ fontSize: '13px' }}>wisdom . love</p>
                 </div>
               </div>
